d0ee5db5959f07f2bd040f218e1e139140dffd86
Vinícius Knob <knob.vinicius@gmail.com>
2022-01-13 14:54:47 -0300

    SaudePetrobras: Copy messages in Recurso de Glosa

--- src/js/pages/saudepetrobras/recurso_glosa_busca_detalhe.page.js
+++ src/js/pages/saudepetrobras/recurso_glosa_busca_detalhe.page.js
@@ -17,6 +17,16 @@
             PATHNAME_REGEX = /recursoglosa\/buscaDetalheRecursoGlosa/;
 
         const
+            _btnCopy_ignoreFields = [
+                'Contrato',
+                'Prestador',
+                'Área',
+                'Código Procedimento',
+                'Quantidade',
+                'Procedimento',
+            ];
+
+        const
             __btnCopy_onclick = function () {
                 Analytics.sendEvent('clickButton', 'log', 'btnCopy');
 
@@ -41,35 +51,80 @@
                         stopLoop = true;
 
                     } else {
-                        let spanElement = label.parentElement.querySelector('span');
-                        value = spanElement ? spanElement.textContent : '';
-                        value = value ? value.replace('R$', '').trim() : '';
+                        if (_btnCopy_ignoreFields.some(item => item == labelText) === false) {
+                            let spanElement = label.parentElement.querySelector('span');
+                            value = spanElement ? spanElement.textContent : '';
+                            value = value ? value.replace('R$', '').trim() : '';
+                        }
+                        else {
+                            return;
+                        }
                     }
 
                     barArr.push(value);
                 });
 
-                var boxMessageList = document.querySelectorAll('.box-resposta-mensagem');
-                boxMessageList.forEach((box) => {
-                    let message = box.querySelector('.span-13.jump-1.size-12.padrao').textContent;
-                    message = message.replace(/\n*/g, '');
-                    barArr.push(message);
+                bazArr.push(barArr.join('\t'));
+
+                Clipboard.write(bazArr.join('\n')).then(() => Snackbar.fire('Copiado!'));
+
+            },
+            __btnCopyMessages_onclick = function () {
+                Analytics.sendEvent('clickButton', 'log', 'btnCopyMessages');
+
+                let labelList = document.querySelectorAll('#body label');
+
+                let stopLoop = false;
+                let resourceNumber = '';
+
+                labelList.forEach((label) => {
+                    if (stopLoop)
+                        return;
+
+                    let labelText = label.textContent;
+                    labelText = labelText ? labelText.replace(':', '').trim() : '';
+
+                    if (/Motivo.+Glosa/.test(labelText))
+                        stopLoop = true;
+
+                    if (/^Recurso/.test(labelText)) {
+                        let spanElement = label.parentElement.querySelector('span');
+                        let value = spanElement ? spanElement.textContent : '';
+                        resourceNumber = value ? value.replace('R$', '').trim() : '';
+                    }
+
                 });
 
-                let barArrJoined = barArr.join('\t');
-                bazArr.push(barArrJoined);
+                let lines = [];
 
-                let bazArrJoined = bazArr.join('\n');
+                var boxMessageList = document.querySelectorAll('.box-resposta-mensagem,.box-resposta-resposta');
+                boxMessageList.forEach((box) => {
+                    let boxp1div = box.querySelector('.box-resposta-p1 div');
+                    let user = boxp1div.querySelector('label').textContent.trim();
+                    let date = boxp1div.querySelector('span').textContent.trim();
+                    let message = box.querySelector('.box-resposta-p2 span').textContent.trim();
+                    message = message.replace(/[\n\t]*/g, '');
 
-                Clipboard.write(bazArrJoined).then(() => Snackbar.fire('Copiado!'));
+                    let line = [ resourceNumber, user, date, message ];
+                    lines.push(line.join('\t'));
+                });
+
+                Clipboard.write(lines.join('\n')).then(() => Snackbar.fire('Copiado!'));
 
             },
             _upgrade = () => {
-                FAB.build([ {
-                    textLabel: 'Copiar dados',
-                    iconClass: 'lar la-copy',
-                    click: __btnCopy_onclick,
-                } ]);
+                FAB.build([
+                    {
+                        textLabel: 'Copiar detalhes',
+                        iconClass: 'lar la-copy',
+                        click: __btnCopy_onclick,
+                    },
+                    {
+                        textLabel: 'Copiar mensagens',
+                        iconClass: 'lar la-copy',
+                        click: __btnCopyMessages_onclick,
+                    },
+                ]);
             },
             _init = () => {
                 if (PATHNAME_REGEX.test(location.pathname))
