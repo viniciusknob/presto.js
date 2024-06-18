73c6aa8a8d5e91abd7d87f5b060c71b0225f8d4f
Vinícius Knob <viniciusknob@compasso.com.br>
2024-06-17 23:06:16 -0300

    keep npm packages updated; add gulp-terser; remove gulp-minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,97 @@
-const { src, dest, series } = require('gulp');
-const fs = require('fs');
-const replace = require('gulp-replace');
-const rename = require('gulp-rename');
-const htmlMinify = require('gulp-htmlmin');
-const cssConcat = require('gulp-concat-css');
-const cssMinify = require('gulp-clean-css');
-const jsConcat = require('gulp-concat');
-const jsMinify = require('gulp-minify');
+const { src, dest, series } = require("gulp");
+const fs = require("fs");
+const replace = require("gulp-replace");
+const rename = require("gulp-rename");
+const htmlMinify = require("gulp-htmlmin");
+const cssConcat = require("gulp-concat-css");
+const cssMinify = require("gulp-clean-css");
+const jsConcat = require("gulp-concat");
+const jsMinify = require("gulp-terser");
 
-const DIST_PATH = 'dist';
-const BUILD_PATH = 'build';
+const DIST_PATH = "dist";
+const BUILD_PATH = "build";
 
 function _htmlMinify() {
-    return src([
-            'src/html/*',
-        ])
-        .pipe(htmlMinify({
-            collapseWhitespace: true,
-            removeComments: true,
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(dest(`${BUILD_PATH}/html`));
+  return src(["src/html/*"])
+    .pipe(
+      htmlMinify({
+        collapseWhitespace: true,
+        removeComments: true,
+      })
+    )
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(dest(`${BUILD_PATH}/html`));
 }
 
-
 function _cssConcat() {
-    return src([
-            'src/css/*',
-        ])
-        .pipe(cssConcat('presto.css'))
-        .pipe(dest(`${BUILD_PATH}/css`));
+  return src(["src/css/*"])
+    .pipe(cssConcat("presto.css"))
+    .pipe(dest(`${BUILD_PATH}/css`));
 }
 
 function _cssMinify() {
-    return src([
-            `${BUILD_PATH}/css/presto.css`
-        ])
-        .pipe(cssMinify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(dest(`${BUILD_PATH}/css`));
+  return src([`${BUILD_PATH}/css/presto.css`])
+    .pipe(cssMinify())
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(dest(`${BUILD_PATH}/css`));
 }
 
 function _minToJS(env) {
-    return () => src(`${DIST_PATH}/presto.js`)
-        .pipe(replace('__env__', env))
-		.pipe(replace('__modal__', fs.readFileSync(`${BUILD_PATH}/html/modal.min.html`, 'utf8')))
-        .pipe(replace('__css__', fs.readFileSync(`${BUILD_PATH}/css/presto.min.css`, 'utf8')))
-        .pipe(dest(DIST_PATH));
+  return () =>
+    src(`${DIST_PATH}/presto.js`)
+      .pipe(replace("__env__", env))
+      .pipe(
+        replace(
+          "__modal__",
+          fs.readFileSync(`${BUILD_PATH}/html/modal.min.html`, "utf8")
+        )
+      )
+      .pipe(
+        replace(
+          "__css__",
+          fs.readFileSync(`${BUILD_PATH}/css/presto.min.css`, "utf8")
+        )
+      )
+      .pipe(dest(DIST_PATH));
 }
 
 function _jsConcat() {
-	return src([
-		'src/js/presto.js',
-		'src/js/analytics.js',
-		'src/js/helpers/*',
-		'src/js/sulamerica.js',
-		'src/js/pages/saudepetrobras/*.js',
-		'src/js/saudepetrobras.js',
-		'src/js/pages/canoasprev/*.js',
-		'src/js/canoasprev.js',
-		'src/js/main.js',
-		'src/js/init.js'
-	])
-	.pipe(jsConcat('presto.js'))
-	.pipe(dest(DIST_PATH));
-};
+  return src([
+    "src/js/presto.js",
+    "src/js/analytics.js",
+    "src/js/helpers/*",
+    "src/js/sulamerica.js",
+    "src/js/pages/saudepetrobras/*.js",
+    "src/js/saudepetrobras.js",
+    "src/js/pages/canoasprev/*.js",
+    "src/js/canoasprev.js",
+    "src/js/main.js",
+    "src/js/init.js",
+  ])
+    .pipe(jsConcat("presto.js"))
+    .pipe(dest(DIST_PATH));
+}
 
 function _jsMinify() {
-	return src([
-		'dist/presto.js'
-	])
-	.pipe(jsMinify({
-		ext: {
-			min:'.min.js'
-		}
-	 }))
-	 .pipe(dest(DIST_PATH));
-};
+  return src(["dist/presto.js"]).pipe(jsMinify()).pipe(dest(DIST_PATH));
+}
 
-const files = env => [
-    _htmlMinify,
-    _cssConcat,
-    _cssMinify,
-    _jsConcat,
-    _minToJS(env),
-    _jsMinify,
-]
+const files = (env) => [
+  _htmlMinify,
+  _cssConcat,
+  _cssMinify,
+  _jsConcat,
+  _minToJS(env),
+  _jsMinify,
+];
 
-exports.default = series(...files('dev'));
-exports.release = series(...files('prd'));
+exports.default = series(...files("dev"));
+exports.release = series(...files("prd"));
